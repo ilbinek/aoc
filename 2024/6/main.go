598bac04d2fef51d0219f46a43919ac256018484
ilbinek <sotirios.pupakis@gmail.com>
2024-12-06 22:35:21 +0100

    smarter 6

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -1,177 +1,233 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
 type direction int
 
 const (
-    up direction = iota
-    right
-    down
-    left
+	up direction = iota
+	right
+	down
+	left
 )
 
 type Pos struct {
-    X   int
-    Y   int
-    Dir direction
+	X   int
+	Y   int
+	Dir direction
 }
 
 type Guard struct {
-    X   int
-    Y   int
-    Dir direction
+	X   int
+	Y   int
+	Dir direction
 }
 
 func loadData(filepath string) ([][]bool, Guard) {
-    file, err := os.Open(filepath)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    ret := make([][]bool, 0)
-    ret2 := Guard{0, 0, right}
-    for scanner.Scan() {
-        row := make([]bool, 0)
-        for _, c := range scanner.Text() {
-            if c == '#' {
-                row = append(row, false)
-            } else if c == '^' {
-                ret2.X = len(row)
-                ret2.Y = len(ret)
-                ret2.Dir = up
-                row = append(row, true)
-            } else {
-                row = append(row, true)
-            }
-        }
-        ret = append(ret, row)
-    }
+	scanner := bufio.NewScanner(file)
+	ret := make([][]bool, 0)
+	ret2 := Guard{0, 0, right}
+	for scanner.Scan() {
+		row := make([]bool, 0)
+		for _, c := range scanner.Text() {
+			if c == '#' {
+				row = append(row, false)
+			} else if c == '^' {
+				ret2.X = len(row)
+				ret2.Y = len(ret)
+				ret2.Dir = up
+				row = append(row, true)
+			} else {
+				row = append(row, true)
+			}
+		}
+		ret = append(ret, row)
+	}
 
-    return ret, ret2
+	return ret, ret2
 }
 
 func turnRight(dir direction) direction {
-    return (dir + 1) % 4
+	return (dir + 1) % 4
 }
 
 func check(maze [][]bool, x int, y int) bool {
-    if x < 0 || x >= len(maze[0]) || y < 0 || y >= len(maze) {
-        return true
-    }
-    return maze[y][x]
+	if x < 0 || x >= len(maze[0]) || y < 0 || y >= len(maze) {
+		return true
+	}
+	return maze[y][x]
 }
 
-func solve(maze [][]bool, guard Guard) int {
-    m := make(map[Pos]bool)
+func solve(maze [][]bool, guard Guard) map[Pos]bool {
+	m := make(map[Pos]bool)
 
-    for {
-        if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
-            return len(m)
-        }
-        m[Pos{guard.X, guard.Y, up}] = true
-        switch guard.Dir {
-        case up:
-            if check(maze, guard.X, guard.Y-1) {
-                guard.Y--
-            } else {
-                guard.Dir = turnRight(guard.Dir)
-            }
-        case right:
-            if check(maze, guard.X+1, guard.Y) {
-                guard.X++
-            } else {
-                guard.Dir = turnRight(guard.Dir)
-            }
-        case down:
-            if check(maze, guard.X, guard.Y+1) {
-                guard.Y++
-            } else {
-                guard.Dir = turnRight(guard.Dir)
-            }
-        case left:
-            if check(maze, guard.X-1, guard.Y) {
-                guard.X--
-            } else {
-                guard.Dir = turnRight(guard.Dir)
-            }
-        }
-    }
+	for {
+		if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
+			return m
+		}
+		m[Pos{guard.X, guard.Y, up}] = true
+		switch guard.Dir {
+		case up:
+			if check(maze, guard.X, guard.Y-1) {
+				guard.Y--
+			} else {
+				guard.Dir = turnRight(guard.Dir)
+			}
+		case right:
+			if check(maze, guard.X+1, guard.Y) {
+				guard.X++
+			} else {
+				guard.Dir = turnRight(guard.Dir)
+			}
+		case down:
+			if check(maze, guard.X, guard.Y+1) {
+				guard.Y++
+			} else {
+				guard.Dir = turnRight(guard.Dir)
+			}
+		case left:
+			if check(maze, guard.X-1, guard.Y) {
+				guard.X--
+			} else {
+				guard.Dir = turnRight(guard.Dir)
+			}
+		}
+	}
 }
 
 func copyMaze(maze [][]bool) [][]bool {
-    ret := make([][]bool, len(maze))
-    for i := 0; i < len(maze); i++ {
-        ret[i] = make([]bool, len(maze[i]))
-        copy(ret[i], maze[i])
-    }
-    return ret
+	ret := make([][]bool, len(maze))
+	for i := 0; i < len(maze); i++ {
+		ret[i] = make([]bool, len(maze[i]))
+		copy(ret[i], maze[i])
+	}
+	return ret
 }
 
 func solve2(maze [][]bool, guard Guard) int {
-    mazeCopy := copyMaze(maze)
-    guardCopy := guard
-    ret := 0
-    for i := 0; i < len(maze); i++ {
-        for j := 0; j < len(maze[i]); j++ {
+	mazeCopy := copyMaze(maze)
+	guardCopy := guard
+	ret := 0
+	for i := 0; i < len(maze); i++ {
+		for j := 0; j < len(maze[i]); j++ {
+			m := make(map[Pos]bool)
+			maze = copyMaze(mazeCopy)
+			guard = guardCopy
+			if maze[i][j] && (i != guard.Y || j != guard.X) {
+				maze[i][j] = false
+			}
+			for {
+				if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
+					break
+				}
+				m[Pos(guard)] = true
+				switch guard.Dir {
+				case up:
+					if check(maze, guard.X, guard.Y-1) {
+						guard.Y--
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				case right:
+					if check(maze, guard.X+1, guard.Y) {
+						guard.X++
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				case down:
+					if check(maze, guard.X, guard.Y+1) {
+						guard.Y++
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				case left:
+					if check(maze, guard.X-1, guard.Y) {
+						guard.X--
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				}
+				if m[Pos(guard)] {
+					ret++
+					break
+				}
+			}
+		}
+	}
+	return ret
+}
 
-            m := make(map[Pos]bool)
-            maze = copyMaze(mazeCopy)
-            guard = guardCopy
-            if maze[i][j] && (i != guard.Y || j != guard.X) {
-                maze[i][j] = false
+func solve3(maze [][]bool, guard Guard, path map[Pos]bool) int {
+	mazeCopy := copyMaze(maze)
+	guardCopy := guard
+	ret := 0
+    delete(path, Pos{guard.X, guard.Y, guard.Dir})
+    for k, _ := range path {
+        m := make(map[Pos]bool)
+		maze = copyMaze(mazeCopy)
+		guard = guardCopy
+        maze[k.Y][k.X] = false
+
+        for {
+            if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
+                break
             }
-            for {
-                if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
-                    break
+            m[Pos(guard)] = true
+            switch guard.Dir {
+            case up:
+                if check(maze, guard.X, guard.Y-1) {
+                    guard.Y--
+                } else {
+                    guard.Dir = turnRight(guard.Dir)
                 }
-                m[Pos(guard)] = true
-                switch guard.Dir {
-                case up:
-                    if check(maze, guard.X, guard.Y-1) {
-                        guard.Y--
-                    } else {
-                        guard.Dir = turnRight(guard.Dir)
-                    }
-                case right:
-                    if check(maze, guard.X+1, guard.Y) {
-                        guard.X++
-                    } else {
-                        guard.Dir = turnRight(guard.Dir)
-                    }
-                case down:
-                    if check(maze, guard.X, guard.Y+1) {
-                        guard.Y++
-                    } else {
-                        guard.Dir = turnRight(guard.Dir)
-                    }
-                case left:
-                    if check(maze, guard.X-1, guard.Y) {
-                        guard.X--
-                    } else {
-                        guard.Dir = turnRight(guard.Dir)
-                    }
+            case right:
+                if check(maze, guard.X+1, guard.Y) {
+                    guard.X++
+                } else {
+                    guard.Dir = turnRight(guard.Dir)
                 }
-                if m[Pos(guard)] {
-                    ret++
-                    break
+            case down:
+                if check(maze, guard.X, guard.Y+1) {
+                    guard.Y++
+                } else {
+                    guard.Dir = turnRight(guard.Dir)
                 }
+            case left:
+                if check(maze, guard.X-1, guard.Y) {
+                    guard.X--
+                } else {
+                    guard.Dir = turnRight(guard.Dir)
+                }
+            }
+            if m[Pos(guard)] {
+                ret++
+                break
             }
         }
     }
-    return ret
+	return ret
 }
 
 func main() {
-    maze, guard := loadData("data.txt")
-
-    fmt.Println(solve(maze, guard))
-    fmt.Println(solve2(maze, guard))
+	maze, guard := loadData("data.txt")
+	path := solve(maze, guard)
+	fmt.Println(len(path))
+	t0 := time.Now()
+	fmt.Println(solve2(maze, guard))
+	fmt.Printf("Bruteforce: %fs\n", time.Since(t0).Seconds())
+	t0 = time.Now()
+	fmt.Println(solve3(maze, guard, path))
+	fmt.Printf("Smarter: %fs\n", time.Since(t0).Seconds())
 }
