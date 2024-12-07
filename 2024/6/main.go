a0517c13665de8108ebcae3d6b66581a7f555c60
ilbinek <sotirios.pupakis@gmail.com>
2024-12-08 00:32:40 +0100

    7

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -169,55 +171,164 @@ func solve2(maze [][]bool, guard Guard) int {
 	return ret
 }
 
+func solve2Multi(maze [][]bool, guard Guard) int {
+	var ret atomic.Int64
+	wg := sync.WaitGroup{}
+	for i := 0; i < len(maze); i++ {
+		for j := 0; j < len(maze[i]); j++ {
+			wg.Add(1)
+			mazeCopy := copyMaze(maze)
+			guardCopy := guard
+			if i != guardCopy.Y || j != guardCopy.X {
+				mazeCopy[i][j] = false
+			}
+			go func(mazeCopy [][]bool, guardCopy Guard) {
+				m := make(map[Pos]bool)
+				for {
+					if guardCopy.X < 0 || guardCopy.X >= len(mazeCopy[0]) || guardCopy.Y < 0 || guardCopy.Y >= len(mazeCopy) {
+						break
+					}
+					m[Pos(guardCopy)] = true
+					switch guardCopy.Dir {
+					case up:
+						if check(mazeCopy, guardCopy.X, guardCopy.Y-1) {
+							guardCopy.Y--
+						} else {
+							guardCopy.Dir = turnRight(guardCopy.Dir)
+						}
+					case right:
+						if check(mazeCopy, guardCopy.X+1, guardCopy.Y) {
+							guardCopy.X++
+						} else {
+							guardCopy.Dir = turnRight(guardCopy.Dir)
+						}
+					case down:
+						if check(mazeCopy, guardCopy.X, guardCopy.Y+1) {
+							guardCopy.Y++
+						} else {
+							guardCopy.Dir = turnRight(guardCopy.Dir)
+						}
+					case left:
+						if check(mazeCopy, guardCopy.X-1, guardCopy.Y) {
+							guardCopy.X--
+						} else {
+							guardCopy.Dir = turnRight(guardCopy.Dir)
+						}
+					}
+					if m[Pos(guardCopy)] {
+						ret.Add(1)
+						break
+					}
+				}
+			}(mazeCopy, guardCopy)
+		}
+	}
+	return int(ret.Load())
+}
+
 func solve3(maze [][]bool, guard Guard, path map[Pos]bool) int {
 	mazeCopy := copyMaze(maze)
 	guardCopy := guard
 	ret := 0
-    delete(path, Pos{guard.X, guard.Y, guard.Dir})
-    for k, _ := range path {
-        m := make(map[Pos]bool)
+	delete(path, Pos(guard))
+	for k, _ := range path {
+		m := make(map[Pos]bool)
 		maze = copyMaze(mazeCopy)
 		guard = guardCopy
-        maze[k.Y][k.X] = false
+		maze[k.Y][k.X] = false
 
-        for {
-            if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
-                break
-            }
-            m[Pos(guard)] = true
-            switch guard.Dir {
-            case up:
-                if check(maze, guard.X, guard.Y-1) {
-                    guard.Y--
-                } else {
-                    guard.Dir = turnRight(guard.Dir)
-                }
-            case right:
-                if check(maze, guard.X+1, guard.Y) {
-                    guard.X++
-                } else {
-                    guard.Dir = turnRight(guard.Dir)
-                }
-            case down:
-                if check(maze, guard.X, guard.Y+1) {
-                    guard.Y++
-                } else {
-                    guard.Dir = turnRight(guard.Dir)
-                }
-            case left:
-                if check(maze, guard.X-1, guard.Y) {
-                    guard.X--
-                } else {
-                    guard.Dir = turnRight(guard.Dir)
-                }
-            }
-            if m[Pos(guard)] {
-                ret++
-                break
-            }
-        }
-    }
+		for {
+			if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
+				break
+			}
+			m[Pos(guard)] = true
+			switch guard.Dir {
+			case up:
+				if check(maze, guard.X, guard.Y-1) {
+					guard.Y--
+				} else {
+					guard.Dir = turnRight(guard.Dir)
+				}
+			case right:
+				if check(maze, guard.X+1, guard.Y) {
+					guard.X++
+				} else {
+					guard.Dir = turnRight(guard.Dir)
+				}
+			case down:
+				if check(maze, guard.X, guard.Y+1) {
+					guard.Y++
+				} else {
+					guard.Dir = turnRight(guard.Dir)
+				}
+			case left:
+				if check(maze, guard.X-1, guard.Y) {
+					guard.X--
+				} else {
+					guard.Dir = turnRight(guard.Dir)
+				}
+			}
+			if m[Pos(guard)] {
+				ret++
+				break
+			}
+		}
+	}
 	return ret
+}
+
+func solve3Multi(maze [][]bool, guard Guard, path map[Pos]bool) int {
+	var ret atomic.Int64
+	wg := sync.WaitGroup{}
+	delete(path, Pos(guard))
+	for k := range path {
+		copyMaze := copyMaze(maze)
+		guardCopy := guard
+		copyMaze[k.Y][k.X] = false
+		wg.Add(1)
+		go func(maze [][]bool, guard Guard) {
+			m := make(map[Pos]bool)
+			for {
+				if guard.X < 0 || guard.X >= len(maze[0]) || guard.Y < 0 || guard.Y >= len(maze) {
+					break
+				}
+				m[Pos(guard)] = true
+				switch guard.Dir {
+				case up:
+					if check(maze, guard.X, guard.Y-1) {
+						guard.Y--
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				case right:
+					if check(maze, guard.X+1, guard.Y) {
+						guard.X++
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				case down:
+					if check(maze, guard.X, guard.Y+1) {
+						guard.Y++
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				case left:
+					if check(maze, guard.X-1, guard.Y) {
+						guard.X--
+					} else {
+						guard.Dir = turnRight(guard.Dir)
+					}
+				}
+				if m[Pos(guard)] {
+					ret.Add(1)
+					break
+				}
+			}
+			wg.Done()
+		}(copyMaze, guardCopy)
+	}
+	wg.Wait()
+	return int(ret.Load())
 }
 
 func main() {
@@ -227,7 +338,13 @@ func main() {
 	t0 := time.Now()
 	fmt.Println(solve2(maze, guard))
 	fmt.Printf("Bruteforce: %fs\n", time.Since(t0).Seconds())
+	//t0 = time.Now()
+	//fmt.Println(solve2Multi(maze, guard))
+	//fmt.Printf("Bruteforce Multi: %fs\n", time.Since(t0).Seconds())
 	t0 = time.Now()
 	fmt.Println(solve3(maze, guard, path))
 	fmt.Printf("Smarter: %fs\n", time.Since(t0).Seconds())
+	t0 = time.Now()
+	fmt.Println(solve3Multi(maze, guard, path))
+	fmt.Printf("Smarter Multi: %fs\n", time.Since(t0).Seconds())
 }
---
+++ 2024/7/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+    "strings"
+)
+
+type Equation struct {
+    Result  int
+    Numbers []int
+}
+
+func loadData(filepath string) []Equation {
+    file, err := os.Open(filepath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    ret := make([]Equation, 0)
+    for scanner.Scan() {
+        parts := strings.Split(scanner.Text(), ":")
+        result, err := strconv.Atoi(parts[0])
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        numbers := make([]int, 0)
+        for _, num := range strings.Split(strings.Trim(parts[1], " "), " ") {
+            n, err := strconv.Atoi(num)
+            if err != nil {
+                log.Fatal(err)
+            }
+            numbers = append(numbers, n)
+        }
+
+        ret = append(ret, Equation{Result: result, Numbers: numbers})
+    }
+
+    return ret
+}
+
+func canCombine(eq Equation) bool {
+    nbrOfOperations := len(eq.Numbers) - 1
+    options := 1 << uint(nbrOfOperations)
+    for i := 0; i < options; i++ {
+        sum := eq.Numbers[0]
+        for j := 0; j < nbrOfOperations; j++ {
+            if i&(1<<uint(j)) != 0 {
+                sum += eq.Numbers[j+1]
+            } else {
+                sum *= eq.Numbers[j+1]
+            }
+        }
+        if sum == eq.Result {
+            return true
+        }
+    }
+    return false
+}
+
+func solve(equations []Equation) int {
+    ret := 0
+    for _, eq := range equations {
+        if canCombine(eq) {
+            ret += eq.Result
+        }
+    }
+    return ret
+}
+
+func combineNumbers(a, b int) int {
+    aStr := strconv.Itoa(a)
+    bStr := strconv.Itoa(b)
+    ret, err := strconv.Atoi(aStr + bStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return ret
+}
+
+func check(equation Equation) bool {
+    if len(equation.Numbers) == 1 {
+        return equation.Numbers[0] == equation.Result
+    }
+    nbrs := []int{equation.Numbers[0] + equation.Numbers[1]};
+    nbrs = append(nbrs, equation.Numbers[2:]...)
+    e := Equation{equation.Result, nbrs}
+    b := check(e)
+    if b {
+        return true
+    }
+
+    nbrs = []int{equation.Numbers[0] * equation.Numbers[1]};
+    nbrs = append(nbrs, equation.Numbers[2:]...)
+    e = Equation{equation.Result, nbrs}
+    b = check(e)
+    if b {
+        return true
+    }
+    
+    nbrs = []int{combineNumbers(equation.Numbers[0], equation.Numbers[1])};
+    nbrs = append(nbrs, equation.Numbers[2:]...)
+    e = Equation{equation.Result, nbrs}
+    b = check(e)
+    return b
+}
+
+func solve22(equations []Equation) int {
+    ret := 0
+    for _, eq := range equations {
+        if check(eq) {
+            ret += eq.Result
+        }
+    }
+    return ret
+}
+
+func main() {
+    equa := loadData("data.txt")
+    fmt.Println(solve(equa))
+    fmt.Println(solve22(equa))
+}
